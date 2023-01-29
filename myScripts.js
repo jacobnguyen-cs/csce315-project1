88606e6c0bbac62d320fe50ce9c66d57d42eec4a
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-01-29 00:40:23 -0600

    Style Toggle Button
    Added a button to toggle the styles.

---
+++ myScripts.js
@@ -0,0 +1,12 @@
+var checkbox = document.querySelector("input[name=checkbox]");
+var link = document.getElementById("style");
+
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("Checkbox is checked...");
+    link.setAttribute("href", "styles2.css");
+  } else {
+    console.log("Not checked");
+    link.setAttribute("href", "styles1.css");
+  }
+})
