22042bea7434b3154e6e1b95db45de39b69bb99e
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-02-06 17:43:48 -0600

    Added Toggle Button.

--- myScripts.js
+++ myScripts.js
@@ -1,13 +1,31 @@
 /* toggle button */
-var checkbox = document.querySelector("input[name=checkbox]");
-var link = document.getElementById("style");
-
-checkbox.addEventListener('change', function() {
-  if (this.checked) {
-    console.log("Checkbox is checked...");
-    link.setAttribute("href", "styles2.css");
+var bool = true;
+function toggle() {
+  bool = !bool;
+  if (bool) {
+    style1();
   } else {
-    console.log("Not checked");
-    link.setAttribute("href", "styles1.css");
+    style2();
   }
-})
+}
+
+function load_style() {
+  page_style = localStorage.getItem("page_style");
+  if (page_style == null) {
+    page_style = "styles1.css";
+  }
+
+  document.getElementById('page_style').setAttribute("href", page_style);
+}
+
+function style1() {
+  localStorage.setItem("page_style", "styles1.css");
+  load_style();
+}
+
+function style2() {
+  localStorage.setItem("page_style", "styles2.css");
+  load_style();
+}
+
+load_style();
